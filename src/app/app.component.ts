71af831e63b65cb74590266e7adcad08c2b08d43
Dzoibo ranites <ivandztitan@gmail.com>
2022-11-09 07:10:59 +0200

    adding chat and upload photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(private menuCtrl: MenuController) {}
+  ngOnInit(): void {
+    this.menuCtrl.toggle();
+  }
+
+  closeMenu(){
+    const toggle=document.getElementById('closeMenu');
+    toggle.click();
+  }
 }
